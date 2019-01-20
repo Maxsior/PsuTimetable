d9716fe06c9f81fa7d8fc92717612dcc4329506c
Алексей <en.aleksey@gmail.com>
2019-01-20 23:56:27 +0500

    Add logout functional

--- PsuTimetable/PsuTimetable/MainPage.xaml.cs
+++ PsuTimetable/PsuTimetable/MainPage.xaml.cs
@@ -9,9 +9,49 @@ namespace PsuTimetable
 {
 	public partial class MainPage : ContentPage
 	{
+		Label messageLabel;
+
 		public MainPage()
 		{
-			InitializeComponent();
+			//InitializeComponent();
+
+			// TO-DO: Check internet connection
+
+			messageLabel = new Label
+			{
+				HorizontalOptions = LayoutOptions.Center
+			};
+
+			var toolbarItem = new ToolbarItem
+			{
+				Text = "Выход"
+			};
+			toolbarItem.Clicked += OnLogoutButtonClicked;
+			ToolbarItems.Add(toolbarItem);
+
+			Title = "Расписание";
+
+			Content = new StackLayout
+			{
+				Children = {
+					new ScrollView {
+						VerticalOptions = LayoutOptions.FillAndExpand,
+						Content = messageLabel
+					}
+				}
+			};
+		}
+
+		async void OnLogoutButtonClicked(object sender, EventArgs e)
+		{
+			await Logout();
+		}
+
+		async Task Logout()
+		{
+			App.IsLoggedIn = false;
+			Navigation.InsertPageBefore(new LoginPage(), this);
+			await Navigation.PopAsync();
 		}
 	}
 }
