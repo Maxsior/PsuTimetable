9168b773575422720e23a1973abdd72f6b13d141
Алексей <en.aleksey@gmail.com>
2019-05-28 15:50:15 +0500

    Scroll to current tab on Tabbar

---
+++ PsuTimetable/PsuTimetable.Android/Renderers/CustomTabbedPageRenderer.cs
@@ -0,0 +1,41 @@
+﻿using Android.Content;
+using Android.OS;
+using Android.Support.Design.Widget;
+using System;
+using PsuTimetable.Controls;
+using PsuTimetable.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.Platform.Android.AppCompat;
+
+
+[assembly: ExportRenderer(typeof(CustomTabbedPage), typeof(CustomTabbedPageRenderer))]
+namespace PsuTimetable.Droid.Renderers
+{
+	public class CustomTabbedPageRenderer : TabbedPageRenderer
+	{
+		private TabLayout tabLayout = null;
+
+		public CustomTabbedPageRenderer(Context context) : base(context) { }
+
+		protected override void OnElementChanged(ElementChangedEventArgs<TabbedPage> e)
+		{
+			base.OnElementChanged(e);
+
+			this.tabLayout = (TabLayout)this.GetChildAt(1);
+
+			var selectPosition = this.tabLayout.SelectedTabPosition;
+
+			tabLayout.TabMode = TabLayout.ModeScrollable;
+			tabLayout.TabGravity = TabLayout.GravityFill;
+
+			Handler h = new Handler();
+			Action myAction = () =>
+			{
+				tabLayout.GetTabAt(selectPosition).Select();
+			};
+
+			h.PostDelayed(myAction, 1);
+		}
+	}
+}
